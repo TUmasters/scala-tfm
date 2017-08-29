4c72c26472426b821ea8864c68c062ea9f03d460
WIlliam <william.a.macke@gmail.com>
2017-08-29 14:47:56 -0500

    Updated the script to use the new command line format me and chad disscussed

--- bin/generateGraphics.py
+++ bin/generateGraphics.py
@@ -3,33 +3,33 @@
 import json
 import sys
 import os
-from time import time
 
-Sstructure = 'data/comments.json'
-Stopics = 'data/plsa/words.json'
-output = str(time())
+#Set default parameter names
+root = 'data/'
+protocol = 'uatfm'
+source = 'comments.json'
 
+#Load parameters from command line arguments
 if len(sys.argv) >= 2:
-  output = sys.argv[1]
+  root = sys.argv[1]
 if len(sys.argv) >= 3:
-  structure = sys.argv[2]
+  protocol = sys.argv[2]
 if len(sys.argv) >= 4:
-  topics = sys.argv[3]
-
-output = 'graphics/'+output
+  source = sys.argv[3]
 
 
-with open(Sstructure,'r') as f:
+#Create output directory in case it doesn't exist
+output = root+'/graphics'
+os.mkdir(output)
+
+
+#Load in json files
+with open(root+source, 'r') as f:
   structure = json.load(f)
 
-with open(Stopics, 'r') as f:
+with open(root+protocol+'/words.json', 'r') as f:
   topics = json.load(f)
 
 topics = topics['documents']
 
-os.mkdir(output)
-
-
-
-convNum = 0
-
+print(structure)
