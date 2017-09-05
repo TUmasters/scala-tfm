ef9555cd22900fca7631d87a711000277359113d
WIlliam <william.a.macke@gmail.com>
2017-09-05 13:55:11 -0500

    added code for coloring conversation graph based on topics

--- bin/generateGraphics.py
+++ bin/generateGraphics.py
@@ -5,13 +5,15 @@ import sys
 import os
 from _utils import *
 
-#Set default parameter names
-root = 'data/'
-protocol = 'plsa'
+colors = ['blue','red','green','yellow','purple','cyan','darkgreen','darksalmon','deeppink','lightseagreen','white']
+
+# Set default parameter names
+root = 'data'
+protocol = 'uatfm'
 source = 'comments.json'
 rawFile = 'raw.json'
 
-#Load parameters from command line arguments
+# Load parameters from command line arguments
 if len(sys.argv) >= 2:
     root = sys.argv[1]
 if len(sys.argv) >= 3:
@@ -22,25 +24,22 @@ if len(sys.argv) >= 5:
     rawFile = sys.argv[4]
 
 
-#Create output directory in case it doesn't exist
-output = root+'graphics/'
+# Create output directory in case it doesn't exist
+output = root+'/graphics/'+protocol+"/"
+if not os.path.isdir(root+'/graphics'):
+    os.mkdir(root+'/graphics')
 if not os.path.isdir(output):
     os.mkdir(output)
 
 
-#Load in json files
-with open(root+source, 'r') as f:
+# Load in json files
+with open(root+'/'+source, 'r') as f:
     dataset = json.load(f)
-
-with open(root+protocol+'/words.json', 'r') as f:
-    topics = json.load(f)
-with open(root+rawFile, 'r') as f:
+with open(root+'/' + rawFile, 'r') as f:
     raw = json.load(f)
 
 
-topics = topics['documents']
-
-#Generate Tree Structures of Conversations
+# Generate Tree Structures of Conversations
 comments = dict([(d['id'], create_comment(d)) for d in dataset])
 for id in comments:
     expand(comments[id], comments)
@@ -48,56 +47,107 @@ roots = list(filter(lambda comment: comment.parent is None, comments.values()))
 
 rawComments = dict([(d['id'], create_comment_from_raw(d)) for d in raw])
 
-for i in range(len(roots)):
-    if i % 100 == 0:
-        print("Working on batch ", int(i/100), "of", int(len(roots)/100), "...")
-    if not os.path.isdir(output+str(i)):
-        os.mkdir(output+str(i))
-    # Generate Graphviz Header
-    graphviz = "digraph G {\n"
-    # Generate Latex Header
-    latexHeader = "\\documentclass{article}\n"
-    latexHeader += "\\usepackage{graphicx}\n"
-    latexHeader += "\\usepackage{floatrow}\n"
-    latexHeader += "\\usepackage{color}\n"
-    # latexHeader += "\\usepacakge{spverbatim}\n"
-    # Create String for Latex Color Definitions
-    latexColor = ""
-    # Generate Latex Document Header
-    latexDoc = "\\begin{document}\n"
-    latexDoc += "\\begin{center}\n"
-    latexDoc += "\\includegraphics{" + output+str(i)+"/conversation.pdf}\n"
-    latexDoc += "\\end{center}\n"
-    commentDict = {roots[i].id: 0}
-    numColors = 0
-    for comment in roots[i]:
-        # Generate Latex Document
-        words = topics[comment.id]
-        latexDoc += "\\begin{spverbatim}\n"+str(commentDict[comment.id]) + " " + rawComments[comment.id].content + "\n\\end{spverbatim}\n"
-        # Make word labels
-        for w in words:
-            while numColors <= w['t']:
-                color = random_color()
-                latexColor += "\\definecolor{color"+str(numColors)+"}{RGB}{"+str(color[0])+", "+str(color[1])+", "+str(color[2])+"}\n"
-                numColors += 1
-            latexDoc += "\\textcolor{color"+str(w['t']) + "}{" + w['w'] + "} "
-        latexDoc += "\\break\\break\n"
-        # Construct Graph
-        for reply in comment.replies:
-            if reply.id not in commentDict:
-                commentDict[reply.id] = len(commentDict)
-            graphviz += "\t" + str(commentDict[comment.id]) + " -> " + str(commentDict[reply.id]) + ";\n"
-    latexDoc += "\\end{document}"
-    graphviz += "}"
-    with open(output+str(i)+"/conversation.dot",'w') as f:
-        f.write(graphviz)
-    latex = latexHeader+latexColor+latexDoc
-    with open(output+str(i)+"/figure.tex", 'w') as f:
-        f.write(latex)
 
-    os.system("dot -Tpdf " + output + str(i) + "/conversation.dot -o " + output + str(i) + "/conversation.pdf")
-    os.system('pdflatex -interaction=nonstopmode -output-directory ' + output+str(i)+"/ " + output+str(i) + "/figure.tex > /dev/null")
-            
-
-
-
+if protocol == 'plsa':
+    with open(root+protocol+'/words.json', 'r') as f:
+        topics = json.load(f)
+    topics = topics['documents']
+    for i in range(len(roots)):
+        if i % 100 == 0:
+            print("Working on batch ", int(i/100), "of", int(len(roots)/100), "...")
+        if not os.path.isdir(output+str(i)):
+            os.mkdir(output+str(i))
+        # Generate Graphviz Header
+        graphviz = "digraph G {\n"
+        # Generate Latex Header
+        latexHeader = "\\documentclass{article}\n"
+        latexHeader += "\\usepackage{graphicx}\n"
+        latexHeader += "\\usepackage{floatrow}\n"
+        latexHeader += "\\usepackage{color}\n"
+        # latexHeader += "\\usepacakge{spverbatim}\n"
+        # Create String for Latex Color Definitions
+        latexColor = ""
+        # Generate Latex Document Header
+        latexDoc = "\\begin{document}\n"
+        latexDoc += "\\begin{center}\n"
+        latexDoc += "\\includegraphics{" + output+str(i)+"/conversation.pdf}\n"
+        latexDoc += "\\end{center}\n"
+        commentDict = {roots[i].id: 0}
+        numColors = 0
+        for comment in roots[i]:
+            # Generate Latex Document
+            words = topics[comment.id]
+            latexDoc += "\\begin{spverbatim}\n"+str(commentDict[comment.id]) + " " + rawComments[comment.id].content + "\n\\end{spverbatim}\n"
+            # Make word labels
+            for w in words:
+                while numColors <= w['t']:
+                    color = random_color()
+                    latexColor += "\\definecolor{color"+str(numColors)+"}{RGB}{"+str(color[0])+", "+str(color[1])+", "+str(color[2])+"}\n"
+                    numColors += 1
+                latexDoc += "\\textcolor{color"+str(w['t']) + "}{" + w['w'] + "} "
+            latexDoc += "\\break\\break\n"
+            # Construct Graph
+            for reply in comment.replies:
+                if reply.id not in commentDict:
+                    commentDict[reply.id] = len(commentDict)
+                graphviz += "\t" + str(commentDict[comment.id]) + " -> " + str(commentDict[reply.id]) + ";\n"
+        latexDoc += "\\end{document}"
+        graphviz += "}"
+        with open(output+str(i)+"/conversation.dot", 'w') as f:
+            f.write(graphviz)
+        latex = latexHeader+latexColor+latexDoc
+        with open(output+str(i)+"/figure.tex", 'w') as f:
+            f.write(latex)
+    
+        os.system("dot -Tpdf " + output + str(i) + "/conversation.dot -o " + output + str(i) + "/conversation.pdf")
+        os.system('pdflatex -interaction=nonstopmode -output-directory ' + output+str(i)+"/ " + output+str(i) + "/figure.tex > /dev/null")
+else:
+    with open(root+'/'+protocol+'/document-topics.json', 'r') as f:
+        documentTopics = json.load(f)
+    with open(root+'/'+protocol+'/word-topics.json', 'r') as f:
+        wordTopics = json.load(f)
+    for i in range(len(roots)):
+        if len(roots[i].replies) < 2: continue
+        if i % 100 == 0:
+            print("Working on batch ", int(i/100), "of", int(len(roots)/100), "...")
+        if not os.path.isdir(output+str(i)):
+            os.mkdir(output+str(i))
+        # Generate Graphviz Header
+        graphviz = "digraph G {\n"
+        # Generate Latex Header
+        latexHeader = "\\documentclass{article}\n"
+        latexHeader += "\\usepackage{graphicx}\n"
+        latexHeader += "\\usepackage{floatrow}\n"
+        latexHeader += "\\usepackage{color}\n"
+        # latexHeader += "\\usepacakge{spverbatim}\n"
+        # Create String for Latex Color Definitions
+        latexColor = ""
+        # Generate Latex Document Header
+        latexDoc = "\\begin{document}\n"
+        latexDoc += "\\begin{center}\n"
+        latexDoc += "\\includegraphics{" + output+str(i)+"/conversation.pdf}\n"
+        latexDoc += "\\end{center}\n"
+        commentDict = {roots[i].id: 0}
+        numColors = 0
+        for comment in roots[i]:
+            # Generate Latex Document
+            latexDoc += "\\begin{spverbatim}\n"+str(commentDict[comment.id]) + " " + rawComments[comment.id].content + "\n\\end{spverbatim}+\\hfill\\break\\hfill\\break\n"
+            # Make word labels
+            # TODO: create word topics
+            # Construct Graph
+            graphviz += "\t" + str(commentDict[comment.id]) + " [style=filled fillcolor = " + colors[documentTopics[comment.id][0]['topic']] + "]\n"
+            for reply in comment.replies:
+                if reply.id not in commentDict:
+                    commentDict[reply.id] = len(commentDict)
+                graphviz += "\t" + str(commentDict[comment.id]) + " -> " + str(commentDict[reply.id]) + ";\n"
+        latexDoc += "\\end{document}"
+        graphviz += "}"
+        with open(output+str(i)+"/conversation.dot", 'w') as f:
+            f.write(graphviz)
+        latex = latexHeader+latexColor+latexDoc
+        with open(output+str(i)+"/figure.tex", 'w') as f:
+            f.write(latex)
+    
+        os.system("dot -Tpdf " + output + str(i) + "/conversation.dot -o " + output + str(i) + "/conversation.pdf")
+        os.system('pdflatex -interaction=nonstopmode -output-directory ' + output+str(i)+"/ " + output+str(i) + "/figure.tex > /dev/null")
+        break
